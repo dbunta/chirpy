64a8d212cf5012def143a004b3d2b23bc57ee49a
dbunta <dmbunta@gmail.com>
2025-05-21 22:30:46 -0400

    more chirpy

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiConfig := apiConfig{}
+	//mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	//mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	handler := handlerMain()
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /healthz", handlerHealthz)
+	mux.HandleFunc("GET /metrics", apiConfig.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiConfig.handlerReset)
 
-	server := http.Server{
+	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
@@ -20,4 +28,37 @@ func main() {
 	if err != nil {
 		fmt.Printf("%w", err)
 	}
+}
+
+func handlerMain() http.Handler {
+	return http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+}
+
+func handlerHealthz(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	cfg.fileServerHits.Store(0)
 }
